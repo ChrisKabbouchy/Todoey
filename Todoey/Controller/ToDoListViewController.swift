e8cd1ff617491b37c891ef6a03ce191a34ff221f
ChrisKabbouchy <57867758+ChrisKabbouchy@users.noreply.github.com>
2019-11-23 22:13:48 +0200

    persisting data using loadable protocol

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController{
     
-    var list = ["move data","cast data","hello data"]
-    let defaults =  UserDefaults.standard
-
+    var list = [ToDoData]()
+    let dataPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("ToDoItem.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let items = defaults.array(forKey: "ToDoItem")  as? [String] {
-            list = items
-
-        }
+        loadData()
+        tableView.reloadData()
         // Do any additional setup after loading the view.
     }
     
@@ -30,13 +27,18 @@ class ToDoListViewController: UITableViewController{
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
+        
         // alert will show what the user whant to do
         let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         let action =  UIAlertAction(title: "add item", style: .default) { (action) in
             // action is the user pressed the button
-            print(textField.text!)
-            self.list.append(textField.text!)
-            self.defaults.set(self.list, forKey: "ToDoItem")
+            
+            let newItem = ToDoData()
+            newItem.item = textField.text!
+            self.list.append(newItem)
+            self.saveData()
+            
+            //self.defaults.set(self.list.last?.item, forKey: "ToDoItem")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
@@ -48,7 +50,32 @@ class ToDoListViewController: UITableViewController{
     }
     
     
-     
+    
+    //MARK: -Save and Load new data from/to codable database
+    func saveData(){
+        
+        let encoder = PropertyListEncoder()
+        do{
+            let data =  try encoder.encode(self.list)
+            try data.write(to: self.dataPath!)
+        }catch{
+            print(error)
+        }
+    }
+    
+    func loadData() {
+        
+        let decoder = PropertyListDecoder()
+        do{
+            let data = try Data(contentsOf: dataPath!)
+            list = try decoder.decode([ToDoData].self, from: data)
+        }catch{
+            
+        }
+    }
+    
+    
+    
     //MARK: -TableView
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -56,17 +83,26 @@ class ToDoListViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row]
+        cell.textLabel?.text = list[indexPath.row].item
+        let item = list[indexPath.row].selected
+        
+        cell.accessoryType = item ? .checkmark : .none
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        list[indexPath.row].selected = !list[indexPath.row].selected
+        saveData()
+        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
+        
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
---
+++ Todoey/Model/ToDoData.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoData.swift
+//  Todoey
+//
+//  Created by Christian Kabouchy on 11/22/19.
+//  Copyright © 2019 Christian Kabouchy. All rights reserved.
+//
+
+import Foundation
+
+class ToDoData : Codable {
+    var item : String = ""
+    var selected : Bool = false
+}
